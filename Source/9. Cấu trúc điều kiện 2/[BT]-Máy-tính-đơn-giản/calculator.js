62cb7b3d935441a2f11cd6ebc5f2f4ad99a9d93e
inhtruong <duydat.dhsp@gmail.com>
2021-05-08 23:31:56 +0700

    Update may tinh don gian

---
+++ Source/9. Cấu trúc điều kiện 2/[BT]-Máy-tính-đơn-giản/calculator.js
@@ -0,0 +1,18 @@
+function view(button){
+    document.getElementById('input').value += button;
+}
+function calculat(){
+    let x = document.getElementById('input').value;   
+    let y = eval(x);
+    document.getElementById('input').value = y;
+    if(y==undefined){
+        alert("Please enter the number");
+        document.getElementById('input').value = "";
+    }
+    
+} 
+function clearnum(){
+    
+    document.getElementById('input').value = "" ;
+}
+  
