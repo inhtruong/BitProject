875e36f3def9a2f851166c79a253d42247076c45
inhtruong <duydat.dhsp@gmail.com>
2021-05-23 08:07:49 +0700

    Update từ điển new

--- Source/15. Hàm 2/[BT]-Từ-Điển/data.js
+++ Source/15. Hàm 2/[BT]-Từ-Điển/data.js
@@ -15,24 +15,27 @@ let vietNam = [
   "trái chuối",
   "quả bơ",
   "quả mít",
-
 ];
 let result;
 function search() {
   let data = document.getElementById("data").value;
   data = clearUnnecessaryWhiteSpace(data);
-    if (engLish.indexOf(data) != -1) {
-        for (i in vietNam) {
-            if (i == engLish.indexOf(data))
-            result = vietNam[i];
-        }
-    } else {
-      result = "Không tìm được từ này!";
-    }
-  
-  document.getElementsByTagName("span")[0].innerHTML = result;
+  dictionary(engLish, vietNam, data);
 }
 
 function clearUnnecessaryWhiteSpace(str) {
-    return str.trim().replace(/  +/g, " ");
+  return str.trim().replace(/  +/g, " ");
+}
+
+function dictionary(arr_1, arr_2, data) {
+  if (arr_1.indexOf(data) != -1) {
+    for (i in arr_2) {
+      if (i == arr_1.indexOf(data)) {
+        result = arr_2[i];
+      }
+    }
+  } else {
+    result = "Không tìm được từ này!";
   }
+  document.getElementsByTagName("span")[0].innerHTML = result;
+}
