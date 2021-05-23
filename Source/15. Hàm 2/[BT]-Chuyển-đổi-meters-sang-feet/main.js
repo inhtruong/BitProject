d04f18de5218cd06d0894d6fdc71bffb4330c3c8
inhtruong <duydat.dhsp@gmail.com>
2021-05-23 08:39:16 +0700

    Update bài chuyển đổi meters sang feet

---
+++ Source/15. Hàm 2/[BT]-Chuyển-đổi-meters-sang-feet/main.js
@@ -0,0 +1,8 @@
+function  meterToFeet(meter){
+    meter = document.getElementById('meter').value;
+     document.getElementById('feet').value =  3.279 * meter;
+ }
+ function  feettToMeter(feet){
+     feet = document.getElementById('feet').value;
+     document.getElementById('meter').value = 0.305 * feet
+ }
